1830d7da864c6526a593434afe6157e6abeef532
Taehyub Kim <taehyub.kim@samsung.com>
2021-03-15 19:23:03 +0900

    renderer: code reordering for readability.

--- src/renderer/src/thorvg_renderer.cpp
+++ src/renderer/src/thorvg_renderer.cpp
@@ -40,6 +40,13 @@ Point applyTransform(const Vec2D &vec, const Mat2D &mat)
 	return {vec[0] * m.e11 + vec[1] * m.e12 + m.e13, vec[0] * m.e21 + vec[1] * m.e22 + m.e23};
 }
 
+void TvgRenderPath::reset()
+{
+   m_Shape->reset();
+   m_PathType.clear();
+   m_PathPoints.clear();
+}
+
 void TvgRenderPath::addRenderPath(RenderPath* path, const Mat2D& transform)
 {
    auto m_PathType = static_cast<TvgRenderPath*>(path)->m_PathType;
@@ -86,13 +93,6 @@ void TvgRenderPath::addRenderPath(RenderPath* path, const Mat2D& transform)
    }
 }
 
-void TvgRenderPath::reset()
-{
-   m_Shape->reset();
-   m_PathType.clear();
-   m_PathPoints.clear();
-}
-
 void TvgRenderPath::moveTo(float x, float y)
 {
    m_PathType.push_back(PathCommand::MoveTo);
@@ -116,58 +116,6 @@ void TvgRenderPath::cubicTo(float ox, float oy, float ix, float iy, float x, flo
 void TvgRenderPath::close()
 {
    m_PathType.push_back(PathCommand::Close);
-}
-
-void TvgRenderer::save()
-{
-}
-
-void TvgRenderer::restore()
-{
-}
-
-void TvgRenderer::transform(const Mat2D& transform)
-{
-   m_Transform = transform;
-}
-
-void TvgRenderer::drawPath(RenderPath* path, RenderPaint* paint)
-{
-   auto renderPath = static_cast<TvgRenderPath*>(path);
-   auto shape = static_cast<TvgRenderPath*>(path)->shape();
-   auto tvgPaint = static_cast<TvgRenderPaint*>(paint)->paint();
-
-   /* OPTIMIZE ME: Stroke / Fill Paints required to draw separately.
-      thorvg doesn't need to handle both, we can avoid one of them rendering... */
-
-   if (tvgPaint->style == RenderPaintStyle::fill)
-   {
-      shape->fill(tvgPaint->color[0], tvgPaint->color[1], tvgPaint->color[2], tvgPaint->color[3]);
-   }
-   else if (tvgPaint->style == RenderPaintStyle::stroke)
-   {
-      shape->stroke(tvgPaint->color[0], tvgPaint->color[1], tvgPaint->color[2], tvgPaint->color[3]);
-      shape->stroke(tvgPaint->cap);
-      shape->stroke(tvgPaint->join);
-      shape->stroke(tvgPaint->thickness);
-   }
-
-   shape->transform({m_Transform[0], m_Transform[2], m_Transform[4], m_Transform[1], m_Transform[3], m_Transform[5], 0, 0, 1});
-
-   if (renderPath->onCanvas())
-   {
-      m_Canvas->update(shape);
-   }
-   else
-   {
-      m_Canvas->push(unique_ptr<Shape>(shape));
-      renderPath->onCanvas(true);
-   }
-}
-
-void TvgRenderer::clipPath(RenderPath* path)
-{
-
 }
 
 TvgRenderPaint::TvgRenderPaint()
@@ -225,6 +173,32 @@ void TvgRenderPaint::cap(StrokeCap value)
    }
 }
 
+void TvgRenderPaint::linearGradient(float sx, float sy, float ex, float ey)
+{
+   m_GradientBuilder = new TvgLinearGradientBuilder(sx, sy, ex, ey);
+}
+
+void TvgRenderPaint::radialGradient(float sx, float sy, float ex, float ey)
+{
+   m_GradientBuilder = new TvgRadialGradientBuilder(sx, sy, ex, ey);
+}
+
+void TvgRenderPaint::addStop(unsigned int color, float stop)
+{
+   m_GradientBuilder->stops.emplace_back(GradientStop(color, stop));
+}
+
+void TvgRenderPaint::completeGradient()
+{
+   m_GradientBuilder->make(&m_Paint);
+   delete m_GradientBuilder;
+}
+
+void TvgRenderPaint::blendMode(BlendMode value)
+{
+
+}
+
 void TvgRadialGradientBuilder::make(TvgPaint* paint)
 {
    /* Implements Tvg Radial Gradient Here*/
@@ -254,28 +228,56 @@ void TvgLinearGradientBuilder::make(TvgPaint* paint)
    }
 }
 
-void TvgRenderPaint::linearGradient(float sx, float sy, float ex, float ey)
+void TvgRenderer::save()
 {
-   m_GradientBuilder = new TvgLinearGradientBuilder(sx, sy, ex, ey);
+
 }
 
-void TvgRenderPaint::radialGradient(float sx, float sy, float ex, float ey)
+void TvgRenderer::restore()
 {
-   m_GradientBuilder = new TvgRadialGradientBuilder(sx, sy, ex, ey);
+
 }
 
-void TvgRenderPaint::addStop(unsigned int color, float stop)
+void TvgRenderer::transform(const Mat2D& transform)
 {
-   m_GradientBuilder->stops.emplace_back(GradientStop(color, stop));
+   m_Transform = transform;
 }
 
-void TvgRenderPaint::completeGradient()
+void TvgRenderer::drawPath(RenderPath* path, RenderPaint* paint)
 {
-   m_GradientBuilder->make(&m_Paint);
-   delete m_GradientBuilder;
+   auto renderPath = static_cast<TvgRenderPath*>(path);
+   auto shape = static_cast<TvgRenderPath*>(path)->shape();
+   auto tvgPaint = static_cast<TvgRenderPaint*>(paint)->paint();
+
+   /* OPTIMIZE ME: Stroke / Fill Paints required to draw separately.
+      thorvg doesn't need to handle both, we can avoid one of them rendering... */
+
+   if (tvgPaint->style == RenderPaintStyle::fill)
+   {
+      shape->fill(tvgPaint->color[0], tvgPaint->color[1], tvgPaint->color[2], tvgPaint->color[3]);
+   }
+   else if (tvgPaint->style == RenderPaintStyle::stroke)
+   {
+      shape->stroke(tvgPaint->color[0], tvgPaint->color[1], tvgPaint->color[2], tvgPaint->color[3]);
+      shape->stroke(tvgPaint->cap);
+      shape->stroke(tvgPaint->join);
+      shape->stroke(tvgPaint->thickness);
+   }
+
+   shape->transform({m_Transform[0], m_Transform[2], m_Transform[4], m_Transform[1], m_Transform[3], m_Transform[5], 0, 0, 1});
+
+   if (renderPath->onCanvas())
+   {
+      m_Canvas->update(shape);
+   }
+   else
+   {
+      m_Canvas->push(unique_ptr<Shape>(shape));
+      renderPath->onCanvas(true);
+   }
 }
 
-void TvgRenderPaint::blendMode(BlendMode value)
+void TvgRenderer::clipPath(RenderPath* path)
 {
 
 }
