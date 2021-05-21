a45ddf966e3dddc399ce9d8c640952e46796c5ed
JunsuChoi <jsuya.choi@samsung.com>
2021-05-21 13:52:45 +0900

    unit test: Set up unit test based on gtest.
    In test directory, set up unit_test based on gtest

---
+++ test/test_controller.cpp
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+class ControllerTest : public ::testing::Test {
+public:
+    void SetUp() {
+    }
+    void TearDown() {
+    }
+public:
+    //Controller controller;
+};
+
+TEST_F(ControllerTest, GenerateController) {
+    //ASSERT_TRUE(controller != nullptr);
+    ASSERT_TRUE(true); //temporary
+}
+
