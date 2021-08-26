c6bb55a9c455594b41554a25a3f1607fef906424
rinechran <rinechran@outlook.jp>
2021-08-27 02:21:53 +0900

    feat : rive_tizen controller implement

--- inc/rive_tizen.hpp
+++ inc/rive_tizen.hpp
@@ -12,15 +12,25 @@ RIVE_EXPORT void rive_tizen_print();
 
 namespace rive_tizen
 {
-   class Controller
-   {
-      rive::Artboard* m_Artboard;
-   public:
-      Controller(){}
-      bool loadFile(char *fileName);
-      bool setTargetBuffer(uint32_t* buffer, int width, int height);
-      bool applyAnimation(char* animationName, bool on);
-      double getDuration();
-      bool render(double time);
-   };
+	class Controller
+	{
+	public:
+		Controller();
+		bool loadFile(const char* fileName);
+		bool setTargetBuffer(uint32_t* buffer, int width, int height);
+		bool applyAnimation(char* animationName, bool on);
+		double getDuration();
+		tvg::SwCanvas* getCanvas();
+		bool render(double time);
+
+		rive::Artboard* getArtboard();
+
+	private:
+		rive::File* m_File;
+		unique_ptr<tvg::SwCanvas> m_Canvas;
+		bool m_Is_Fileloaded;
+
+		int m_Width;
+		int m_Height;
+	};
 }
