51bc2210862c9b4b201f12d21e63c4e88373ebdd
rinechran <rinechran@outlook.jp>
2021-08-27 02:21:53 +0900

    feat : rive_tizen controller implement

--- src/rive_tizen.cpp
+++ src/rive_tizen.cpp
@@ -1,42 +1,120 @@
-#include "rive_tizen.hpp"
+#include <fstream>
+#include <vector>
+#include <cstddef>
+#include <algorithm>
+#include <iterator>
 
+#include "rive_tizen.hpp"
 using namespace rive_tizen;
 
 void rive_tizen_print()
 {
-   // This line to check calling Rive APIs
-   rive::AABB aabb(0, 0, 100, 100);
-   // This line to check rive_tizen API calls
-   std::cout << "hello rive-tizen" << std::endl;
+	// This line to check calling Rive APIs
+	rive::AABB aabb(0, 0, 100, 100);
+	// This line to check rive_tizen API calls
+	std::cout << "hello rive-tizen" << std::endl;
 }
 
-bool Controller::loadFile(char *fileName)
+
+Controller::Controller() : m_Is_Fileloaded(false), m_File(nullptr) {
+}
+
+bool Controller::loadFile(const char* fileName)
 {
-   //TODO: Implements code for loading rive files
-   std::cout << "load File" << std::endl;
-   return true;
+	if (m_File != NULL)
+	{
+		delete m_File;
+		m_File = nullptr;
+	}
+	m_Is_Fileloaded = false;
+	m_Canvas->clear();
+
+	std::ifstream fp(fileName, std::ios::binary);
+
+	if (fp.is_open() == false)
+	{
+		return false;
+	}
+
+	std::vector<uint8_t> binaryData(std::istreambuf_iterator<char>(fp), {});
+
+	std::size_t length = binaryData.size();
+
+	auto reader = rive::BinaryReader(binaryData.data(), length);
+	auto result = rive::File::import(reader, &m_File);
+
+	if (result != rive::ImportResult::success)
+	{
+		fprintf(stderr, "failed to import %s\n", fileName);
+		return false;
+	}
+
+	m_Is_Fileloaded = true;
+	return true;
 }
 
 bool Controller::setTargetBuffer(uint32_t* buffer, int width, int height)
 {
-   //TODO: Implements code for setting target buffer
-   return true;
+	m_Width = width;
+	m_Height = height;
+	m_Canvas = tvg::SwCanvas::gen();
+
+	m_Canvas->target(buffer, width, width, height, tvg::SwCanvas::ARGB8888);
+	//TODO: Implements code for setting target buffer
+	return true;
 }
 
 bool Controller::applyAnimation(char* animationName, bool on)
 {
-   //TODO: Implements code for applying rive animation
-   return true;
+	//TODO: Implements code for applying rive animation
+	return true;
 }
 
 double Controller::getDuration()
 {
-   //TODO: Implements code for Getting rive animation duration
-   return true;
+	auto artboard = getArtboard();
+	if (artboard == nullptr) {
+		return NAN;
+	}
+	auto animation = artboard->firstAnimation();
+	int start = animation->enableWorkArea() ? animation->workStart() : 0;
+	int end =
+		animation->enableWorkArea() ? animation->workEnd() : animation->duration();
+	int range = end - start;
+
+	return range / animation->fps();
 }
 
-bool Controller::render(double time)
+tvg::SwCanvas* rive_tizen::Controller::getCanvas() {
+	return m_Canvas.get();
+}
+
+bool Controller::render(double elapsed)
 {
-   //TODO: Implements code for Rendering rive animation by time
-   return true;
+	m_Canvas->clear();
+
+	auto artboard = this->getArtboard();
+	if (artboard == nullptr)
+	{
+		return false;
+	}
+	artboard->advance(elapsed);
+
+	rive::TvgRenderer renderer(this->getCanvas());
+	renderer.save();
+	renderer.align(rive::Fit::contain,
+		rive::Alignment::center,
+		rive::AABB(0, 0, m_Width, m_Height),
+		artboard->bounds());
+	artboard->draw(&renderer);
+	renderer.restore();
+
+	return true;
+}
+rive::Artboard* Controller::getArtboard() {
+	if (m_File == nullptr)
+	{
+		return nullptr;
+	}
+	return m_File->artboard();
 }
