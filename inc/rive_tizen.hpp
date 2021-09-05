f62cf0bc8a60bc4293855150e926163d446ac57e
Hermet Park <chuneon.park@samsung.com>
2021-09-05 11:59:57 +0900

    rive tizen controller (#66)
    * controller: add rive-tizen controller skeletone codes.
    
    * feat : rive_tizen controller implement
    
    Co-authored-by: Taehyub Kim <taehyub.kim@samsung.com>
    Co-authored-by: rinechran <rinechran@outlook.jp>

--- inc/rive_tizen.hpp
+++ inc/rive_tizen.hpp
@@ -1,5 +1,36 @@
 #include <iostream>
 
+#include "file.hpp"
+#include "math/aabb.hpp"
+#include "artboard.hpp"
+#include "animation/linear_animation_instance.hpp"
+#include "tvg_renderer.hpp"
+
 #define RIVE_EXPORT __attribute__ ((visibility ("default")))
 
 RIVE_EXPORT void rive_tizen_print();
+
+namespace rive_tizen
+{
+	class Controller
+	{
+	public:
+		Controller();
+		bool loadFile(const char* fileName);
+		bool setTargetBuffer(uint32_t* buffer, int width, int height);
+		bool applyAnimation(char* animationName, bool on);
+		double getDuration();
+		tvg::SwCanvas* getCanvas();
+		bool render(double time);
+
+		rive::Artboard* getArtboard();
+
+	private:
+		rive::File* m_File;
+		unique_ptr<tvg::SwCanvas> m_Canvas;
+		bool m_Is_Fileloaded;
+
+		int m_Width;
+		int m_Height;
+	};
+}
--- src/rive_tizen.cpp
+++ src/rive_tizen.cpp
@@ -1,10 +1,120 @@
+#include <fstream>
+#include <vector>
+#include <cstddef>
+#include <algorithm>
+#include <iterator>
+
 #include "rive_tizen.hpp"
-#include "math/aabb.hpp"
+using namespace rive_tizen;
 
 void rive_tizen_print()
 {
-   // This line to check calling Rive APIs
-   rive::AABB aabb(0, 0, 100, 100);
-   // This line to check rive_tizen API calls
-   std::cout << "hello rive-tizen" << std::endl;
+	// This line to check calling Rive APIs
+	rive::AABB aabb(0, 0, 100, 100);
+	// This line to check rive_tizen API calls
+	std::cout << "hello rive-tizen" << std::endl;
+}
+
+
+Controller::Controller() : m_Is_Fileloaded(false), m_File(nullptr) {
+}
+
+bool Controller::loadFile(const char* fileName)
+{
+	if (m_File != NULL)
+	{
+		delete m_File;
+		m_File = nullptr;
+	}
+	m_Is_Fileloaded = false;
+	m_Canvas->clear();
+
+	std::ifstream fp(fileName, std::ios::binary);
+
+	if (fp.is_open() == false)
+	{
+		return false;
+	}
+
+	std::vector<uint8_t> binaryData(std::istreambuf_iterator<char>(fp), {});
+
+	std::size_t length = binaryData.size();
+
+	auto reader = rive::BinaryReader(binaryData.data(), length);
+	auto result = rive::File::import(reader, &m_File);
+
+	if (result != rive::ImportResult::success)
+	{
+		fprintf(stderr, "failed to import %s\n", fileName);
+		return false;
+	}
+
+	m_Is_Fileloaded = true;
+	return true;
+}
+
+bool Controller::setTargetBuffer(uint32_t* buffer, int width, int height)
+{
+	m_Width = width;
+	m_Height = height;
+	m_Canvas = tvg::SwCanvas::gen();
+
+	m_Canvas->target(buffer, width, width, height, tvg::SwCanvas::ARGB8888);
+	//TODO: Implements code for setting target buffer
+	return true;
+}
+
+bool Controller::applyAnimation(char* animationName, bool on)
+{
+	//TODO: Implements code for applying rive animation
+	return true;
+}
+
+double Controller::getDuration()
+{
+	auto artboard = getArtboard();
+	if (artboard == nullptr) {
+		return NAN;
+	}
+	auto animation = artboard->firstAnimation();
+	int start = animation->enableWorkArea() ? animation->workStart() : 0;
+	int end =
+		animation->enableWorkArea() ? animation->workEnd() : animation->duration();
+	int range = end - start;
+
+	return range / animation->fps();
+}
+
+tvg::SwCanvas* rive_tizen::Controller::getCanvas() {
+	return m_Canvas.get();
+}
+
+bool Controller::render(double elapsed)
+{
+	m_Canvas->clear();
+
+	auto artboard = this->getArtboard();
+	if (artboard == nullptr)
+	{
+		return false;
+	}
+	artboard->advance(elapsed);
+
+	rive::TvgRenderer renderer(this->getCanvas());
+	renderer.save();
+	renderer.align(rive::Fit::contain,
+		rive::Alignment::center,
+		rive::AABB(0, 0, m_Width, m_Height),
+		artboard->bounds());
+	artboard->draw(&renderer);
+	renderer.restore();
+
+	return true;
+}
+rive::Artboard* Controller::getArtboard() {
+	if (m_File == nullptr)
+	{
+		return nullptr;
+	}
+	return m_File->artboard();
 }
